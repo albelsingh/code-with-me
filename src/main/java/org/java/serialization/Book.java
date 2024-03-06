714d5479f7ed2f2494534f5fbef240afdc9ff631
albel <albelsingh31@gmail.com>
2024-03-06 15:20:03 +0530

    updating codes

---
+++ src/main/java/org/java/serialization/Book.java
@@ -0,0 +1,50 @@
+package org.java.serialization;
+
+import lombok.Data;
+import org.springframework.util.Assert;
+
+import java.io.*;
+
+@Data
+public class Book implements Serializable {
+    private static final long serialVersionUID = -2936687026040726549L;
+    /*The Role of serialVersionUID. serialVersionUID is a unique identifier for each Serializable class.
+    It is used to verify that the saved object and the loaded class are compatible in terms of serialization.
+    If a deserialized object does not have a matching serialVersionUID , an InvalidClassException is thrown*/
+    private String bookName;
+    private transient String description;
+    private transient int copies;
+    private final transient String bookCategory = "Fiction";
+
+    public static void serialize(Book book) throws Exception {
+        File fileName=new File("Serialization.text");
+        FileOutputStream file = new FileOutputStream(fileName);
+        ObjectOutputStream out = new ObjectOutputStream(file);
+        out.writeObject(book);
+        out.close();
+        file.close();
+    }
+
+    public static Book deserialize() throws Exception {
+        File fileName=new File("Serialization.text");
+        FileInputStream file = new FileInputStream(fileName);
+        ObjectInputStream in = new ObjectInputStream(file);
+        Book book = (Book) in.readObject();
+        in.close();
+        file.close();
+        fileName.deleteOnExit();
+        return book;
+    }
+
+    // getters and setters
+    public static void main(String[] args) throws Exception {
+        Book book = new Book();
+        book.setBookName("Java Reference");
+        book.setDescription("will not be saved");
+        book.setCopies(25);
+        serialize(book);
+        Book deBook= deserialize();
+        System.out.println(deBook+" version"+deBook.serialVersionUID);
+
+    }
+}
--- src/main/java/org/java/util/StringUtils.java
+++ src/main/java/org/java/util/StringUtils.java
@@ -1,9 +1,37 @@
 package org.java.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringUtils {
+    private static void sortZeroAndOne(int[] arr) {
+        int i=0;
+        int j=arr.length-1;
+        int temp;
+        while(i<j){
+            if(arr[i] ==1){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j--;
+            }else {
+                i++;
+            }
+        }
+        Arrays.stream(arr).forEach(System.out::print);
+    }
+    private static void replaceFirstChar(List<String> list) {
+        String name = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            char newChar = name.charAt(i + 1);
+            list.set(i, str.replace(str.charAt(0), newChar));
+        }
+        System.out.println(list);
+    }
+
 
     public static void main(String[] args) {
         // List<String> list = Arrays.asList("name1","name2","name3");
@@ -13,16 +41,25 @@ public class StringUtils {
         list.add("name3");
         //name1->aame1,name2->mame2,name3->eame3
         replaceFirstChar(list);
-    }
-
-    private static void replaceFirstChar(List<String> list) {
-        String name = list.get(0);
-        System.out.println(list.size());
-        for (int i = 0; i < list.size(); i++) {
-            String str = list.get(i);
-            char newChar = name.charAt(i + 1);
-            list.set(i, str.replace(str.charAt(0), newChar));
-        }
-        System.out.println(list);
+        /*
+        Input: N = 5, arr[ ] = {1,0,1,1,0}
+        Output: {0,0,1,1,1}
+        // Time Complexity : O(n)
+        // Space Complexity : O(1)
+         */
+        int arr[]={1,0,1,1,0,1,1,0};
+        sortZeroAndOne(arr);
+        System.out.println("");
+        //Input [1,2,3]  => Output [[1,2],[2,3],[3,4]]
+        List<Integer> integerList= Arrays.asList(1,2,3,4);
+        List<Integer> list1=integerList.subList(0,2);
+        List<Integer> list2=integerList.subList(1,3);
+        List<Integer> list3=integerList.subList(2,4);
+        List<List<Integer>> mainList=new ArrayList<>();
+        mainList.add(list1);
+        mainList.add(list2);
+        mainList.add(list3);
+        System.out.println(mainList);
+        System.out.println(mainList.stream().flatMap(l->l.stream()).collect(Collectors.toList()));
     }
 }
